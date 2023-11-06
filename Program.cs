8b8abd9b703f01daa15a6fa98326c177f8325779
mohongen <496585582@qq.com>
2023-11-06 23:10:35 +0800

    feat: init project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Hello {
+  static void Main() {
+    var s = new Acme.Collections.Stack<int>();
+    s.Push(1);
+    s.Push(10);
+    s.Push(100);
+    Console.WriteLine(s.Pop()); // stack contains 1, 10
+    Console.WriteLine(s.Pop()); // stack contains 1
+    Console.WriteLine(s.Pop()); // stack is empty
+    Console.WriteLine("Hello World");
+  }
+}
